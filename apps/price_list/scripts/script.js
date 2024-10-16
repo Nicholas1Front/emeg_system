c6dcd6115a657917c412c70413f7ac6c33ca90f3
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-10-16 17:58:34 -0300

    init createInputSuggestions function and his first commands

--- apps/price_list/scripts/script.js
+++ apps/price_list/scripts/script.js
@@ -1,3 +1,4 @@
+//get services from services.json
 let services_array = [];
 
 async function getServicesData(){
@@ -5,8 +6,6 @@ async function getServicesData(){
         const response = await fetch("../backend/data/services.json");
 
         const resultArray = await response.json();
-
-        console.log(resultArray);
 
         resultArray.sort((a,b)=>{
             if(a.name < b.name){
@@ -21,9 +20,65 @@ async function getServicesData(){
         });
 
         return resultArray;
+
     }catch(error){
         console.error(`Failed to load json : ${error}`);
     }
 }
 
-getServicesData();
+async function initialize_services_array(){
+    services_array = await getServicesData();
+
+    return services_array;
+}
+
+initialize_services_array();
+
+// show or hide elements
+
+async function showHtmlElement([...elements], displayType){
+    elements.forEach((element)=>{
+        element.style.display = `${displayType}`;
+    })
+}
+
+async function hideHtmlElement([...elements]){
+    elements.forEach((element)=>{
+        element.style.display = `none`;
+    })
+}
+
+// consult-price-list section
+
+// elements
+const consultPriceListSection = document.querySelector(".consult-price-list-section");
+let serviceType_selectSearch = document.querySelector("#service-type_select-search");
+let serviceNameInputSearch = document.querySelector("#service-name-input-search");
+let serviceNameOptionsControl = document.querySelector(".service-name-options-control");
+const consultPriceListBtn = document.querySelector("#consult-price-list_search-btn")
+
+// functions
+
+async function createInputSuggestions(){
+    let servicesSearched = [];
+    serviceNameOptionsControl.innerHTML = "";
+
+    if(serviceType_selectSearch.value === ""){
+        services_array.forEach((element)=>{
+            servicesSearched.push(element.name);
+        })
+        
+        servicesSearched.forEach((service)=>{
+            let serviceNameOption = document.createElement("div");
+
+            serviceNameOption.className = "service-name-option";
+            serviceNameOption.innerHTML = service;
+
+            serviceNameOptionsControl.appendChild(serviceNameOption);
+        })
+    }
+}
+
+setTimeout(()=>{
+    createInputSuggestions();
+},1000)
