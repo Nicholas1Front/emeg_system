610ab00424f38c286bcc01964d5cd8a30eba0459
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-10-16 09:54:30 -0300

    create getServicesData function

--- apps/price_list/scripts/script.js
+++ apps/price_list/scripts/script.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+let services_array = [];
+
+async function getServicesData(){
+    try{
+        const response = await fetch("../backend/data/services.json");
+
+        const resultArray = await response.json();
+
+        console.log(resultArray);
+
+        resultArray.sort((a,b)=>{
+            if(a.name < b.name){
+                return -1;
+            }
+    
+            if(a.name > b.name){
+                return 1; 
+            }
+    
+            return 0;
+        });
+
+        return resultArray;
+    }catch(error){
+        console.error(`Failed to load json : ${error}`);
+    }
+}
+
+getServicesData();
