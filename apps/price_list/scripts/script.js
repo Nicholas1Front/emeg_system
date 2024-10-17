ca1426cad866f423402f8ab3a0d077ab062db8c2
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2024-10-17 17:42:52 -0300

    trying to create a search method for services

--- apps/price_list/scripts/script.js
+++ apps/price_list/scripts/script.js
@@ -60,25 +60,76 @@ const consultPriceListBtn = document.querySelector("#consult-price-list_search-b
 // functions
 
 async function createInputSuggestions(){
+    let allServices = [];
     let servicesSearched = [];
     serviceNameOptionsControl.innerHTML = "";
 
-    if(serviceType_selectSearch.value === ""){
+    if(serviceType_selectSearch.value !== ""){
         services_array.forEach((element)=>{
-            servicesSearched.push(element.name);
+            if(serviceType_selectSearch.value === element.type){
+                allServices.push(element.name);
+            }
         })
-        
-        servicesSearched.forEach((service)=>{
-            let serviceNameOption = document.createElement("div");
 
-            serviceNameOption.className = "service-name-option";
-            serviceNameOption.innerHTML = service;
+        for(let i = 0; i < allServices.length ; i++){
+            if(allServices[i].includes(serviceNameInputSearch.value)){
+                servicesSearched.push(allServices[i]);
+            }
+        }
 
-            serviceNameOptionsControl.appendChild(serviceNameOption);
+        console.log(servicesSearched);
+
+    }else{
+        services_array.forEach((element)=>{
+            if(serviceType_selectSearch.value === element.type){
+                allServices.push(element.name);
+            }
         })
+
+        for(let i = 0; i < allServices.length ; i++){
+            if(allServices[i].includes(serviceNameInputSearch.value)){
+                servicesSearched.push(allServices[i]);
+            }
+        }
     }
+
+    if(servicesSearched.length === 0){
+        serviceNameOptionsControl.innerHTML = "";
+        hideHtmlElement([serviceNameOptionsControl]);
+        return;
+    }
+
+    servicesSearched.forEach((service)=>{
+        let serviceNameOption = document.createElement("div");
+
+        serviceNameOption.className = "service-name-option";
+        serviceNameOption.innerHTML = service;
+
+        serviceNameOptionsControl.appendChild(serviceNameOption);
+    });
+
+    let serviceNameOption = document.querySelectorAll(".service-name-option");
+
+   for(let i = 0 ; i < serviceNameOption.length ; i++){
+        serviceNameOption[i].addEventListener("click",()=>{
+            serviceNameInputSearch.value = serviceNameOption[i].innerHTML;
+            hideHtmlElement([serviceNameOption]);
+        })
+   }
+
+   showHtmlElement([serviceNameOptionsControl],"block");
+
+   document.addEventListener("click",()=>{
+        serviceNameOptionsControl.innerHTML = "";
+        hideHtmlElement([serviceNameOptionsControl]);
+   })
+  
 }
 
-setTimeout(()=>{
-    createInputSuggestions();
-},1000)
+// event listerners
+
+serviceNameInputSearch.addEventListener("input",async()=>{
+    serviceNameInputSearch.value = serviceNameInputSearch.value.toUpperCase();
+
+    await createInputSuggestions();
+})
