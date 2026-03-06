8525d7adca75e1313274a0956925c5964562ad33
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2026-03-06 14:55:08 -0300

    dev_service : add equipaments no service

--- backend/src/modules/dev/dev_service.js
+++ backend/src/modules/dev/dev_service.js
@@ -2,7 +2,7 @@ const knex = require('../../database/knex');
 
 class DevService{
     async resetDatabase(){
-        await knex.raw(`TRUNCATE TABLE users, clients, clients_contacts, notes, attachments RESTART IDENTITY CASCADE`);
+        await knex.raw(`TRUNCATE TABLE users, clients, clients_contacts, notes, attachments, equipaments RESTART IDENTITY CASCADE`);
 
         return { message : 'Database has been reset successfully.' };
     }
