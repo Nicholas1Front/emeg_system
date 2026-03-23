b32d9ed0a905984b78c8e1d2ac06eba756f4650a
Nicholas1Front <nicholasdeoliveiraeugenio@gmail.com>
2026-03-23 09:11:17 -0300

    adição de outras tables em dev service

--- backend/src/modules/dev/dev_service.js
+++ backend/src/modules/dev/dev_service.js
@@ -2,7 +2,7 @@ const knex = require('../../database/knex');
 
 class DevService{
     async resetDatabase(){
-        await knex.raw(`TRUNCATE TABLE users, clients, clients_contacts, notes, attachments, equipaments, items RESTART IDENTITY CASCADE`);
+        await knex.raw(`TRUNCATE TABLE users, clients, clients_contacts, notes, attachments, equipaments, items, budgets, work_orders, budget_items, work_order_items RESTART IDENTITY CASCADE`);
 
         return { message : 'Database has been reset successfully.' };
     }
